9ca0ab7aebca274ed5dfbd4c3408c0b94c1eb00d
Keivon Arooji <karooji0@email.cpcc.edu>
2017-10-12 21:27:16 -0400

    updated html (section), updated js (append)

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,10 +21,21 @@ shopClient.createCart().then(function (newCart) {
 //Retrieve all products (test)
 shopClient.fetchAllProducts().then(products => {
     console.log(products);
-    /*Append all products to html element
+    //Append all products to html element
     for (var i = 0; i < products.length; i++) {
-        $("#")
-    }*/
+        //Create a new div to hold the product
+        var newSmoothie = $("<div>");
+        //Create a new image element with specified dimensions
+        var newImage = $("<img width = '350' height = '350'>");
+        //Add necessary classes
+        newImage.addClass("smoothie");
+        //Set image source to currently iterated product
+        newImage.attr("src", products[i].attrs.images[i].src);
+        //Append new attributes to the div that holds the product
+        newSmoothie.append(newImage, products[i].attrs.title);
+        //Append the newSmoothie div to the section on the HTML file
+        $("section").append(newSmoothie);
+    }
 });
 
 //Retrieve product based on ID (test)
@@ -32,10 +43,11 @@ shopClient.fetchProduct('176946315293').then(product => {
     console.log(product);
 });
 
-/*If user clicks Add To Cart button
-    $(".add-button").on("click", function(){
+//If user clicks Add To Cart button
+    /*$(".add-button").on("click", function(){
         
     });*/
+
 //Add items to the cart
     //Get product.selectedVariant, quantity selected (.val() from HTML)
     /* Example code ...
