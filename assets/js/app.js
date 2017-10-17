9f744bbdce47ebddaa5f8709a8ffb233ef61b03a
Keivon Arooji <karooji0@email.cpcc.edu>
2017-10-16 23:04:40 -0400

    js: descriptions, detail/add btn, click event

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,85 +7,97 @@ var shopClient = ShopifyBuy.buildClient({
     domain: 'fresh-gatherer.myshopify.com',
     //General Buy button ID
     appId: '6'
-  });
-  console.log(shopClient);
-
-//Create a cart
-var newCart;
-shopClient.createCart().then(function (newCart) {
-//   cart = newCart;
-  console.log(newCart.attrs);
-  // do something with updated cart
-  $("#total").text(newCart.subtotal);
 });
 
-//Retrieve all products (test)
-shopClient.fetchAllProducts().then(products => {
-    console.log(products);
-    //Append all products to html element
-    for (var i = 0; i < products.length; i++) {
-
-        //Create a new div to hold the product
-        var newSmoothie = $("<div>");
-
-        // Create a new dive to hold smoothie description
-        var smoothieInfo = $("<div>").addClass("infoDiv reveal");
-
-        // Create a button to show smoothie description
-        var smoothieButton = $("<button>").addClass("moreBtn").text("See More");
+//When document is ready...
+$(document).ready(function(){
         
-        // create h4 for smoothie title
-        var smoothieTitle = $("<h6>").addClass("title");
+    //Create a cart
+    var newCart;
+    shopClient.createCart().then(function (newCart) {
+        console.log(newCart.attrs);
+        // do something with updated cart
+        $("#total").text(newCart.subtotal);
+    });
 
-        smoothieTitle.html(products[i].attrs.title)
-        // add description to smoothie div
-        smoothieInfo.html(products[i].attrs.body_html);
-
-        //Create a new image element with specified dimensions
-
-        var newImage = $("<img width = '350' height = '350'>");
-
-        //Add necessary classes
-        newImage.addClass("smoothie");
-
-        //Set image source to currently iterated product
-        newImage.attr("src", products[i].attrs.images[i].src);
-
-        //Append new attributes to the div that holds the product
-        newSmoothie.append(newImage, smoothieTitle, smoothieButton, smoothieInfo);
-
-        //Append the newSmoothie div to the section on the HTML file
-        $("section").append(newSmoothie);
-    }
-});
-
-//Retrieve product based on ID (test)
-shopClient.fetchProduct('176946315293').then(product => {
-    console.log(product);
-});
-
-//If user clicks Add To Cart button
-    /*$(".add-button").on("click", function(){
+    //Retrieve all products available on the Shopify store
+    shopClient.fetchAllProducts().then(products => {
+        console.log(products);
         
+        //Append all products to HTML element
+        for (var x = 0; x < products.length; x++) {
+            
+            //New image element with smoothie class and correct image source (Always .image[0].src)
+            var newImage = $("<img>").addClass("smoothie").attr("src", products[x].attrs.images[0].src);
+            
+            //h6 for smoothie title
+            var smoothieTitle = $("<h6>").html(products[x].attrs.title);
+
+            //New variable to hold smoothie description
+            var smoothieInfo = $("<p>").html(products[x].attrs.body_html);
+
+            //Description button, Add button
+            var smoothieButton = $("<button>").addClass("btn btn-outline-primary moreBtn").html("Details");
+            var addBtn = $("<button>").addClass("btn btn-outline-success addBtn").html("Select");
+
+            //Append new attributes to a div that contains the entire product
+            var newSmoothie = $("<div>").append(newImage, smoothieTitle, smoothieInfo, smoothieButton, addBtn);
+
+            //Append the newSmoothie div to the section on the HTML file
+            $("section").append(newSmoothie);
+        }
+
+    });
+
+    //On  any button click...
+    $(document).on("click", "button", function(){
+        console.log(this);
+    });
+
+    /*Retrieve product based on ID (test)
+    shopClient.fetchProduct('176946315293').then(product => {
+        console.log(product);
     });*/
 
-//Add items to the cart
-    //Get product.selectedVariant, quantity selected (.val() from HTML)
-    /* Example code ...
-        cart.createLineItemsFromVariants({variant: product.selectedVariant, quantity: 1}).then(function (cart) {
-            // do something with updated cart
-        }); 
-    */
+    //Add items to the cart
+        //Get product.selectedVariant, quantity selected (.val() from HTML)
+        /* Example code ...
+            cart.createLineItemsFromVariants({variant: product.selectedVariant, quantity: 1}).then(function (cart) {
+                // do something with updated cart
+            }); 
+        */
 
-//Checkout with updated cart
-    //Use cart.checkoutURL to generate a checkout URL with current cart
-    /*Example Code
-        document.location.href = cart.checkoutUrl;
-        //Implement Shopify Sandbox for card info
-    */
+    //Checkout with updated cart
+        //Use cart.checkoutURL to generate a checkout URL with current cart
+        /*Example Code
+            document.location.href = cart.checkoutUrl;
+            //Implement Shopify Sandbox for card info
+        */
+    /**********
+     
+    JS Effect
 
-/**********
- 
-  JS Effect
+    *********/ 
+    window.smoothScroll = function(target) {
+        var scrollContainer = target;
+        do { //find scroll container
+            scrollContainer = scrollContainer.parentNode;
+            if (!scrollContainer) return;
+            scrollContainer.scrollTop += 1;
+        } while (scrollContainer.scrollTop == 0);
+        var targetY = 0;
+        do { //find the top of target relatively to the container
+            if (target == scrollContainer) break;
+            targetY += target.offsetTop;
+        } while (target = target.offsetParent);
+        scroll = function(c, a, b, i) {
+            i++; if (i > 30) return;
+            c.scrollTop = a + (b - a) / 30 * i;
+            setTimeout(function(){ scroll(c, a, b, i); }, 20);
+        }
+        // start scrolling
+        scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+    }
 
- *********/ 
+
+});
