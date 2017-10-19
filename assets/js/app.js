bd0cd65ad2851b834c7c0dfe063a03e6a7249841
adamtre9115 <treadam9115@gmail.com>
2017-10-19 18:01:15 -0400

    add back to top and display smoothies in grid layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,14 @@ var shopClient = ShopifyBuy.buildClient({
 
 //When document is ready...
 $(document).ready(function(){
-        
+    
+    //Store user's plan type (required quantity)
+    var plan = 0;
+    //Store total quantity selected
+    var totalQuantity = 0;
+    //Store selected quantity (amount per item)
+    var selectedQuantity = 0;
+
     //Create a cart
     var newCart;
     shopClient.createCart().then(function (newCart) {
@@ -27,29 +34,83 @@ $(document).ready(function(){
         //Append all products to HTML element
         for (var x = 0; x < products.length; x++) {
             
-            //New image element with smoothie class and correct image source (Always .image[0].src)
-            var newImage = $("<img>").addClass("smoothie").attr("src", products[x].attrs.images[0].src);
-            
-            //h6 for smoothie title
+            //New image element with smoothie class, image src, modal attributes, and product information
+            var newSmoothie = $("<img>").addClass("smoothie img-fluid").attr({
+                "src": products[x].attrs.images[0].src,
+                "data-toggle": "modal",
+                "data-target": ".bd-example-modal-lg",
+                "smoothie-ID": products[x].attrs.product_id,
+                "variant-ID" : products[x].attrs.variants[0].id,
+                "in-stock"   : products[x].attrs.variants[0].available,
+                "smoothie-title"  : products[x].attrs.title,
+                "smoothie-details": products[x].attrs.body_html
+            });
+
+            //h6 for the smoothie title
             var smoothieTitle = $("<h6>").html(products[x].attrs.title);
 
-            //New variable to hold smoothie description
-            var smoothieInfo = $("<p>").html(products[x].attrs.body_html);
-
-            //Description button, Add button
-            var smoothieButton = $("<button>").addClass("btn btn-outline-primary moreBtn").html("Details");
-            var addBtn = $("<button>").addClass("btn btn-outline-success addBtn").html("Select");
-
-            //Append new attributes to a div that contains the entire product
-            var newSmoothie = $("<div>").append(newImage, smoothieTitle, smoothieInfo, smoothieButton, addBtn);
+            //Append newSmoothie to a div that contains the entire product
+            var newDiv = $("<div class='col-md-3'>").append(newSmoothie, smoothieTitle);
 
             //Append the newSmoothie div to the section on the HTML file
-            $("section").append(newSmoothie);
+            $("#shopifyImg").append(newDiv);
         }
 
-    });
+        //On subscription click...
+        $(document).on("click", ".plan-btn", function(){
+            //update user's subscription
+            plan = $(this).attr("data-value");
+            //update plan-btn innerHTML
+            //...
+        });
 
-    
+        //On smoothie click...
+        $(document).on("click", ".smoothie", function(){
+            //update the modal content
+                //Image, product/variant ID, availability
+                $(".modal-img").attr({
+                    "src": $(this).attr("src"),
+                    "class": "img-fluid",
+                    "smoothie-ID": $(this).attr("smoothie-ID"),
+                    "variant-ID" : $(this).attr("variant-ID"),
+                    "in-stock"   : $(this).attr("in-stock")
+                });
+                //Title
+                $(".modal-title").html($(this).attr("smoothie-title"));
+                //Details
+                $(".modal-details").html($(this).attr("smoothie-details"));
+        });
+
+        //On add click...
+        $(document).on("click", ".add-btn", function(){
+            //If the item is in stock...(true)
+            if($(".modal-img").attr("in-stock")){
+                //get product ID
+                console.log($(".modal-img").attr("smoothie-ID"));
+                //get variant ID
+                console.log($(".modal-img").attr("variant-ID"));
+                //get product quantity
+                selectedQuantity = parseInt($("#quantity").val());
+                console.log(selectedQuantity);
+                //add to total quantity
+                totalQuantity += selectedQuantity;
+                //add product variant/qty to cart
+                //...
+            }
+            else{
+                console.log("Item is not in stock.");
+            }
+        });
+
+        //On checkout click...
+        $(document).on("click", ".checkout-btn", function(){
+            console.log(this);
+            //If user meets plan qty and plan != 0...
+                //user can click checkout btn
+                //generate checkout URL
+        });
+
+    });
 
     /*Retrieve product based on ID (test)
     shopClient.fetchProduct('176946315293').then(product => {
